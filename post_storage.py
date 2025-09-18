45382b83f00b9d78a54528de2c5623c79e7a8c29
Friedemann Muecket <friedemann@muecket.de>
2025-09-18 15:41:18 +0200

    added docstrings to post_storage.py

--- post_storage.py
+++ post_storage.py
@@ -3,7 +3,7 @@ import os
 
 
 def load_file(file_path):
-    """loads json and returns as list of dicts"""
+    """loads json and returns as list of dicts. If there is no json yet of file is empty return empty list"""
     if not os.path.exists(file_path):
         return []
 
@@ -27,6 +27,7 @@ def get_posts():
 
 
 def get_new_post_id():
+    """ Goes through all dicts of a list and looks for the highest id. Returns new id."""
     posts = get_posts()
     post_id = []
     # Store all ids in list to get max id
@@ -51,21 +52,23 @@ def add_post(author, title, content):
 
 
 def delete_post(post_id):
+    """ takes in post id and deletes dictionary with this id"""
     posts = get_posts()
     for post in posts:
         if post["id"] == post_id:
             posts.remove(post)
-
     write_file(posts)
 
 
 def fetch_post_by_id(post_id):
+    """ takes in an id and returns associated dictionary (post)."""
     posts = get_posts()
     for post in posts:
         if post["id"] == post_id:
             return post
 
 def update_post(author, title, content, post_id):
+    """ takes in author, title, content and id and updates post with id"""
     posts = get_posts()
     for post in posts:
         if post["id"] == post_id:
